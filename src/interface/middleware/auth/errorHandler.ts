c6da8b1e5b1279368fd585875bdf0f0eaedc5877
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-08 00:51:48 +0530

    fix: replace console error logging with Winston in errorHandler

--- src/interface/middleware/auth/errorHandler.ts
+++ src/interface/middleware/auth/errorHandler.ts
@@ -3,41 +3,38 @@ import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../../../shared/constants/ht
 import { z } from "zod";
 import AppError from "../../../shared/utils/AppError";
 import { clearAuthCookies, REFRESH_PATH } from "../../../shared/utils/setAuthCookies";
+import logger from "../../../shared/utils/logger";
 
-
-const handleZodError = (res:Response , error: z.ZodError) => {
-  const errors = error.issues.map((err)=>({
-    path : err.path.join("."),
-    message : err.message
+const handleZodError = (res: Response, error: z.ZodError) => {
+  const errors = error.issues.map((err) => ({
+    path: err.path.join("."),
+    message: err.message,
   }));
   return res.status(BAD_REQUEST).json({
-    errors
+    errors,
   });
-}
+};
 
-
-const handleAppError =(res:Response , error: AppError) => {
+const handleAppError = (res: Response, error: AppError) => {
   return res.status(error.statusCode).json({
     message: error.message,
-    errorCode: error.errorCode
+    errorCode: error.errorCode,
   });
-}
-
+};
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
-    console.log(`Path: ${req.path} , Method: ${req.method} , Error: ${error}`);
-    if(req.path === REFRESH_PATH){
-      clearAuthCookies(res);
-    }
-    if(error instanceof z.ZodError){
-      return handleZodError(res , error);
-    }
-    if(error instanceof AppError){
-      return handleAppError(res, error)
-    }
+  logger.error(`Path: ${req.path} , Method: ${req.method} , Error: ${error}`);
+  if (req.path === REFRESH_PATH) {
+    clearAuthCookies(res);
+  }
+  if (error instanceof z.ZodError) {
+    return handleZodError(res, error);
+  }
+  if (error instanceof AppError) {
+    return handleAppError(res, error);
+  }
 
-    res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
-  };
-  
-  export default errorHandler;
-  
+  res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
+};
+
+export default errorHandler;
