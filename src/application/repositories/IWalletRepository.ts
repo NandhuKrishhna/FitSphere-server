a32fedb1d934bec8e3391bf9670f473c8d162aa4
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-10 21:25:37 +0530

    feat : added find wallet by id  and decrease wallet balance by id

--- src/application/repositories/IWalletRepository.ts
+++ src/application/repositories/IWalletRepository.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
-import { Wallet } from "../../infrastructure/models/walletModel";
+import { Wallet, WalletDocument } from "../../infrastructure/models/walletModel";
 import { Token } from "typedi";
+import { ObjectId } from "../../infrastructure/models/UserModel";
 
 export interface IWalletRepository {
-    createWallet(data : Wallet): Promise<Wallet>;
-    increaseBalance(userId : mongoose.Types.ObjectId, amount : number): Promise<Wallet | null>;
-    addTransaction(userId : mongoose.Types.ObjectId, data : any): Promise<Wallet | null>;
-    getWalletDetailsById(userId : mongoose.Types.ObjectId, data : any): Promise<Wallet | null>;
-};
-
+  createWallet(data: Wallet): Promise<Wallet>;
+  increaseBalance(userId: mongoose.Types.ObjectId, amount: number): Promise<Wallet | null>;
+  addTransaction(userId: mongoose.Types.ObjectId, data: any): Promise<Wallet | null>;
+  getWalletDetailsById(userId: mongoose.Types.ObjectId, data: any): Promise<Wallet | null>;
+  findWalletById(userId: ObjectId): Promise<WalletDocument | null>;
+  decreaseBalance(userId: mongoose.Types.ObjectId, amount: number): Promise<Wallet | null>;
+}
 
 export const IWalletRepositoryToken = new Token<IWalletRepository>();
