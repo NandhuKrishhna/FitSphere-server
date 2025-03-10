2c89644c3122b919369b45fc1adaa00d28088db9
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-10 21:30:35 +0530

    refactor : impliment all the logic  in the controller itself

--- src/infrastructure/repositories/PremiumSubscription.ts
+++ src/infrastructure/repositories/PremiumSubscription.ts
@@ -8,23 +8,14 @@ import { ObjectId } from "../models/UserModel";
 
 @Service(IPremiumSubscriptionRepositoryToken)
 export class PremiumSubscriptionRepository implements IPremiumSubscriptionRepository {
-  async subscribeOne(type: string, price: number, userId: ObjectId): Promise<IPremiumSubscription> {
-    const startDate = new Date();
-    const endDate = new Date();
-    endDate.setMonth(endDate.getMonth() + 1);
-    const subscriptionInfo = new PremiumSubscriptionModel({
-      userId,
-      type,
-      planName: `${type.charAt(0).toUpperCase() + type.slice(1)} Plan`,
-      price,
-      currency: "INR",
-      startDate,
-      endDate,
-      status: "active",
-      autoRenew: false,
-    });
-    await subscriptionInfo.save();
+  async createSubscription(subcription: Partial<IPremiumSubscription>): Promise<IPremiumSubscription> {
+    return await PremiumSubscriptionModel.create(subcription);
+  }
 
-    return subscriptionInfo;
+  async getSubscriptionById(subscriptionId: ObjectId): Promise<IPremiumSubscription | null> {
+    return await PremiumSubscriptionModel.findOne({ _id: subscriptionId });
+  }
+  async getSubscriptionByUserId(userId: ObjectId): Promise<IPremiumSubscription | null> {
+    return await PremiumSubscriptionModel.findOne({ userId: userId });
   }
 }
