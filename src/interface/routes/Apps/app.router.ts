f07f4b4ff7ad9befa5a34096a2a9e36d9767a3eb
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-11 16:56:55 +0530

    chore(router): update routes and controllers for payment & appointment handling

--- src/interface/routes/Apps/app.router.ts
+++ src/interface/routes/Apps/app.router.ts
@@ -2,26 +2,32 @@ import { Router } from "express";
 import Container from "typedi";
 import { AppController } from "../../controllers/Feat/AppController";
 import { ChatController } from "../../controllers/Feat/ChatController";
+import { PaymentController } from "../../controllers/Feat/PaymentController";
 
 const appRouter = Router();
 
 const appController = Container.get(AppController);
 const chatController = Container.get(ChatController);
+const paymentController = Container.get(PaymentController);
 appRouter.get("/doctors/all", appController.displayAllDoctorsHandler);
 appRouter.post("/update-profile", appController.updateProfileHandler);
 appRouter.post("/doctor/profile", appController.doctorDetailsHandler);
 appRouter.post("/doctor/slots", appController.getSlotsHandler);
-appRouter.post("/book/slots", appController.bookAppointment);
-appRouter.post("/verify/payment", appController.verifyPaymentHandler);
 appRouter.post("/get-appointments", appController.getAppointmentHandlers);
-appRouter.post("/cancel/appointments", appController.cancelAppointmentHandler);
-appRouter.post("/payment-failure", appController.abortPaymentHandler);
 appRouter.post("/wallet", appController.getWalletHandler);
+
 // chat
 appRouter.post("/send-message", chatController.sendMessageHandler);
 appRouter.post("/conversation", chatController.getMessagesHandler);
 appRouter.get("/get-users", chatController.getAllUsersHandler);
-appRouter.post("/wallet-payment", appController.walletPaymentHandler);
+
+//payment [wallet, transactions, book appointment]
+appRouter.post("/book/slots", paymentController.bookAppointment);
+appRouter.post("/wallet-payment", paymentController.walletPaymentHandler);
+appRouter.post("/verify/payment", paymentController.verifyPaymentHandler);
+appRouter.post("/payment-failure", paymentController.abortPaymentHandler);
+appRouter.post("/cancel/appointments", paymentController.cancelAppointmentHandler);
+
 // appRouter.post("/start-conversation", chatController.addUsersInSideBarHandler);
 
 export default appRouter;
