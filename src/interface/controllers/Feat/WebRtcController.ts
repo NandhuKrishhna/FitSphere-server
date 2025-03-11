91e5920372e2732c13443452cc2055182f9a88b8
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-11 20:54:22 +0530

    feat(webrtc): implement user joining the video call session with doctor

--- src/interface/controllers/Feat/WebRtcController.ts
+++ src/interface/controllers/Feat/WebRtcController.ts
@@ -4,20 +4,23 @@ import appAssert from "../../../shared/utils/appAssert";
 import { NOT_FOUND, OK } from "../../../shared/constants/http";
 import { Inject, Service } from "typedi";
 import { WebRtcUseCase } from "../../../application/user-casers/WebRtcUseCase";
+import { AuthenticatedRequest } from "../../middleware/auth/authMiddleware";
+import logger from "../../../shared/utils/logger";
 @Service()
 export class WebRtcController {
   constructor(@Inject() private webRtcUseCase: WebRtcUseCase) {}
 
   videoMeetingHandler = catchErrors(async (req: Request, res: Response) => {
-    const meetingId = req.params.meetingId;
-    appAssert(meetingId, NOT_FOUND, "Meeting id is required");
-    const appointment = await this.webRtcUseCase.videoMeeting(meetingId);
+    logger.info("From video meeting handler", req.body);
+    const meetingId = req.body.meetingId;
+    const { userId } = req as AuthenticatedRequest;
+    const { role } = req as AuthenticatedRequest;
+    appAssert(meetingId, NOT_FOUND, "Meeting id is required. Or invalid meeting id");
+    const appointment = await this.webRtcUseCase.videoMeeting(meetingId, userId, role);
     return res.status(OK).json({
       success: true,
+      message: "Successfully joined the meeting",
       meetingId: appointment.meetingId,
-      doctorId: appointment.doctorId,
-      patientId: appointment.patientId,
-      slotId: appointment.slotId,
     });
   });
 }
