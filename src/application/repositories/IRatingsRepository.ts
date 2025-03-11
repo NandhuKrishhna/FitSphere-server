7c5dca461a495259e4adf19e39eb979a3beaf160
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-12 01:10:35 +0530

    feat: create interface for RatingRepository

---
+++ src/application/repositories/IRatingsRepository.ts
@@ -0,0 +1,12 @@
+import { Token } from "typedi";
+import { RatingParams } from "../../domain/types/reviewsAndrating";
+import { IReview } from "../../infrastructure/models/ReviewModel";
+import { ObjectId } from "../../infrastructure/models/UserModel";
+import { IRating } from "../../infrastructure/models/RatingsModel";
+
+export interface IRatingRepository {
+  updateRating({ doctorId, averageRating, totalReviews }: RatingParams): Promise<void>;
+  findRatingByDoctorId(doctorId: ObjectId): Promise<IRating | null>;
+}
+
+export const IRatingRepositoryToken = new Token<IRatingRepository>();
