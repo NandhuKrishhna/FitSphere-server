819dd53e6b2cbc574aee03290f95778e787fc655
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-12 14:12:48 +0530

    refactor(SlotModel): remove unused fields and add timestamps to SlotSchema

--- src/infrastructure/models/slot.models.ts
+++ src/infrastructure/models/slot.models.ts
@@ -1,7 +1,6 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
 
 export interface SlotDocument extends Document {
-  _id: mongoose.Types.ObjectId;
   doctorId: mongoose.Types.ObjectId;
   startTime: Date;
   endTime: Date;
@@ -9,8 +8,6 @@ export interface SlotDocument extends Document {
   consultationType: "video" | "audio" | "chat";
   status: "available" | "booked" | "cancelled" | "completed" | "expired";
   patientId?: mongoose.Types.ObjectId;
-  createdAt?: Date;
-  updatedAt?: Date;
 }
 
 interface SlotModel extends Model<SlotDocument> {
@@ -18,25 +15,26 @@ interface SlotModel extends Model<SlotDocument> {
   deleteExpiredSlots: () => Promise<void>;
 }
 
-const SlotSchema = new Schema<SlotDocument>({
-  doctorId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  startTime: { type: Date, required: true },
-  endTime: { type: Date, required: true },
-  date: { type: Date, required: true },
-  status: {
-    type: String,
-    enum: ["available", "booked", "cancelled", "completed", "expired"],
-    default: "available",
+const SlotSchema = new Schema<SlotDocument>(
+  {
+    doctorId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    date: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["available", "booked", "cancelled", "completed", "expired"],
+      default: "available",
+    },
+    consultationType: {
+      type: String,
+      enum: ["video", "audio", "chat"],
+      default: "video",
+    },
+    patientId: { type: mongoose.Schema.Types.ObjectId, required: false },
   },
-  consultationType: {
-    type: String,
-    enum: ["video", "audio", "chat"],
-    default: "video",
-  },
-  patientId: { type: mongoose.Schema.Types.ObjectId, required: false },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 SlotSchema.statics.updateExpiredSlots = async function () {
   const currentUTC = new Date();
