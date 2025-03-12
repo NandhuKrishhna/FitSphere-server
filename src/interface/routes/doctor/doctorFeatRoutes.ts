ee9e652022f1a8357266b5f55eb0a5277db760a0
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-12 19:48:17 +0530

    feat(DoctorFeatRouter): add route for fetching reviews and ratings

--- src/interface/routes/doctor/doctorFeatRoutes.ts
+++ src/interface/routes/doctor/doctorFeatRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { Container } from "typedi";
 import { ChatController } from "../../controllers/Feat/ChatController";
 import { DoctorFeatController } from "../../controllers/doctor/DoctorFeatController";
+import { AppController } from "../../controllers/Feat/AppController";
 
 const doctorFeatRouter = Router();
 
 const doctorFeatController = Container.get(DoctorFeatController);
 const chatController = Container.get(ChatController);
+const appController = Container.get(AppController);
 
 doctorFeatRouter.post("/slot-management", doctorFeatController.slotManagementHandler);
 doctorFeatRouter.get("/get-slots", doctorFeatController.displayAllSlotsHandler);
@@ -15,5 +17,6 @@ doctorFeatRouter.post("/get/all-appointments", doctorFeatController.getAllAppoin
 doctorFeatRouter.get("/get-all-chats", doctorFeatController.getUsersInSideBarHandler);
 doctorFeatRouter.post("/get-all-messages", chatController.getMessagesHandler);
 doctorFeatRouter.post("/send-message", chatController.sendMessageHandler);
+doctorFeatRouter.get("/get-reviews", appController.fetchReviewsAndRatingHandler);
 
 export default doctorFeatRouter;
