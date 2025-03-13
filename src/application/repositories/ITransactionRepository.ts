5f9595d80bee43d957bf91ddde47e8fd5f72adf8
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-13 20:59:48 +0530

    feat(ITransactionRepository): update getTransactionById return type and add updateTransaction method

--- src/application/repositories/ITransactionRepository.ts
+++ src/application/repositories/ITransactionRepository.ts
@@ -3,7 +3,11 @@ import { ITransaction } from "../../infrastructure/models/transactionModel";
 
 export interface ITransactionRepository {
   createTransaction(transaction: Partial<ITransaction>): Promise<ITransaction>;
-  getTransactionById(transactionId: string): Promise<ITransaction[] | null>;
+  getTransactionById(transactionId: string): Promise<ITransaction | null>;
+  updateTransaction(
+    query: { transactionId?: string; paymentGatewayId?: string; bookingId?: string },
+    update: Partial<ITransaction>
+  ): Promise<ITransaction | null>;
 }
 
 export const ITransactionRepositoryToken = new Token<ITransactionRepository>();
