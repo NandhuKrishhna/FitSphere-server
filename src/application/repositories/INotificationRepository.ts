918b5dfb465c395e595b669fd643aa1958ab0489
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-13 20:59:24 +0530

    feat(INotificationRepository): add markNotificationAsRead method for notification management

--- src/application/repositories/INotificationRepository.ts
+++ src/application/repositories/INotificationRepository.ts
@@ -1,12 +1,14 @@
 import { Token } from "typedi";
 import mongoose from "mongoose";
 import { INotification } from "../../infrastructure/models/notification.models";
+import { ObjectId } from "../../infrastructure/models/UserModel";
 
 export interface INotificationRepository {
   createNotification(notification: Partial<INotification>): Promise<INotification>;
   getAllNotifications(type: string[]): Promise<any>;
   deleteNotification(id: mongoose.Types.ObjectId): Promise<void>;
   getAllNotificationById(id: mongoose.Types.ObjectId): Promise<INotification[]>;
+  markNotificationAsRead(id: ObjectId): Promise<void>;
 }
 
 export const INotificationRepositoryToken = new Token<INotificationRepository>();
