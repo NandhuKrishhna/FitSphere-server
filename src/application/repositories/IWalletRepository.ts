240979dd09f133e7e0710c3e2fd179ec9f8cf56c
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-13 20:59:58 +0530

    feat(IWalletRepository): update methods to use WalletParams and improve type definitions

--- src/application/repositories/IWalletRepository.ts
+++ src/application/repositories/IWalletRepository.ts
@@ -1,15 +1,30 @@
 import mongoose from "mongoose";
-import { Wallet, WalletDocument } from "../../infrastructure/models/walletModel";
+import { WalletDocument } from "../../infrastructure/models/walletModel";
 import { Token } from "typedi";
 import { ObjectId } from "../../infrastructure/models/UserModel";
-
+export type WalletParams = {
+  userId: ObjectId;
+  role : string,
+  amount: number;
+  description?: string;
+  relatedTransactionId?: string
+}
 export interface IWalletRepository {
-  createWallet(data: Wallet): Promise<Wallet>;
-  increaseBalance(userId: mongoose.Types.ObjectId, amount: number): Promise<Wallet | null>;
-  addTransaction(userId: mongoose.Types.ObjectId, data: any): Promise<Wallet | null>;
-  getWalletDetailsById(userId: mongoose.Types.ObjectId, data: any): Promise<Wallet | null>;
-  findWalletById(userId: ObjectId): Promise<WalletDocument | null>;
-  decreaseBalance(userId: mongoose.Types.ObjectId, amount: number): Promise<Wallet | null>;
+  createWallet(data: Partial<WalletDocument>): Promise<WalletDocument>;
+
+  increaseBalance(data: WalletParams):Promise<WalletDocument | null>;
+
+  getWalletDetailsById(
+    userId: mongoose.Types.ObjectId,
+    data: any
+  ): Promise<WalletDocument | null>;
+
+  findWalletById(
+    userId: ObjectId,
+    role: "User" | "Doctor"
+  ): Promise<WalletDocument | null>;
+
+  decreaseBalance(data: WalletParams):Promise<WalletDocument | null>;
 }
 
 export const IWalletRepositoryToken = new Token<IWalletRepository>();
