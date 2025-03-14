2a129c920e3bb083de340bce50265b6775dc7ec8
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-14 21:57:51 +0530

    feat(NotificationModel): update notification schema to include role field and add payment type

--- src/infrastructure/models/notification.models.ts
+++ src/infrastructure/models/notification.models.ts
@@ -1,21 +1,21 @@
 import mongoose, { Schema, Document } from "mongoose";
-
 export interface INotification extends Document {
   userId?: mongoose.Types.ObjectId;
-  type: "doctor_registration" | "general" | "chat" | "appointment";
+  role: "user" | "doctor" | "admin";
+  type: "doctor_registration" | "general" | "chat" | "appointment" | "payment";
   message: string;
   status?: "pending" | "approved" | "rejected";
   metadata?: Record<string, any>;
   read?: boolean;
-  createdAt?: Date;
 }
 
 const NotificationSchema = new Schema<INotification>(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
-    type: { type: String, enum: ["doctor_registration", "general", "chat", "appointment"], required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: false },
+    role: { type: String, enum: ["user", "doctor", "admin"], required: true },
+    type: { type: String, enum: ["doctor_registration", "general", "chat", "appointment", "payment"], required: true },
     message: { type: String, required: true },
-    status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
+    status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" }, // TODO not needed clear this
     metadata: { type: Schema.Types.Mixed },
     read: { type: Boolean, default: false },
   },
