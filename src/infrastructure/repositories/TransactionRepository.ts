dcd6960b75bd2c3e924f8f637b01a833a5c9717b
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-14 21:57:08 +0530

    feat(TransactionRepository): update createTransaction to accept partial transactions and add updateTransaction and getAllTransactions methods

--- src/infrastructure/repositories/TransactionRepository.ts
+++ src/infrastructure/repositories/TransactionRepository.ts
@@ -4,16 +4,33 @@ import {
   ITransactionRepositoryToken,
 } from "../../application/repositories/ITransactionRepository";
 import TransactionModel, { ITransaction } from "../models/transactionModel";
+import { ObjectId } from "../models/UserModel";
 
 @Service(ITransactionRepositoryToken)
 export class TransactionRepository implements ITransactionRepository {
-  async createTransaction(transaction: ITransaction): Promise<ITransaction> {
+  async createTransaction(transaction: Partial<ITransaction>): Promise<ITransaction> {
     const newTransaction = await TransactionModel.create(transaction);
     return newTransaction;
   }
 
-  async getTransactionById(transactionId: string): Promise<ITransaction[] | null> {
-    const transaction = await TransactionModel.find({ transactionId });
+  async getTransactionById(transactionId: string): Promise<ITransaction | null> {
+    const transaction = await TransactionModel.findOne({ transactionId }).exec();
     return transaction;
   }
+
+  async updateTransaction(
+    query: { transactionId?: string; paymentGatewayId?: string; bookingId?: string },
+    update: Partial<ITransaction>
+  ): Promise<ITransaction | null> {
+    const updatedTransaction = await TransactionModel.findOneAndUpdate(query, { $set: update }, { new: true }).exec();
+    return updatedTransaction;
+  }
+  async getAllTransactions(userId: ObjectId): Promise<ITransaction[]> {
+    const transactions = await TransactionModel.find({
+      $or: [{ from: userId }, { to: userId }]
+    }).exec();
+    
+    return transactions;
+  }
+  
 }
