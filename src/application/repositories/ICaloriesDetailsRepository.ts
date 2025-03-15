7e82ea6793853ba353e88249d06ae5d42984163a
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-16 00:06:15 +0530

    feat(ICaloriesDetailsRepository): add editFoodLog method for updating food logs

--- src/application/repositories/ICaloriesDetailsRepository.ts
+++ src/application/repositories/ICaloriesDetailsRepository.ts
@@ -3,8 +3,7 @@ import { IUserDetails } from "../../infrastructure/models/user.addition.details"
 import mongoose from "mongoose";
 import { TUserDetails } from "../../domain/types/calories.Types";
 import { ObjectId } from "../../infrastructure/models/UserModel";
-import { IFoodItem } from "../../infrastructure/models/food.logs";
-import { ICalorieIntake } from "../../infrastructure/models/caloriesIntakeModel";
+import { ICalorieIntake, IFoodItem } from "../../infrastructure/models/caloriesIntakeModel";
 
 export interface ICaloriesDetailsRepository {
   createCaloriesDetails(userId: mongoose.Types.ObjectId, data: TUserDetails): Promise<IUserDetails>;
@@ -12,6 +11,7 @@ export interface ICaloriesDetailsRepository {
   getFoodLogs(userId: ObjectId, date?: Date): Promise<ICalorieIntake | null>;
   getUserHealthDetails(userId: ObjectId): Promise<IUserDetails | null>;
   deleteFoodLogByFoodId(userId: ObjectId, foodId: ObjectId, date: Date): Promise<void>;
+  editFoodLog(userId: ObjectId, foodId: ObjectId, date: Date, updatedFoodItem: IFoodItem, mealType: string): Promise<void>;
 }
 
 export const ICaloriesDetailsRepositoryToken = new Token<ICaloriesDetailsRepository>();
