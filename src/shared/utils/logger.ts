ebd242d76122756266b3e131ba8932c2b461f5c0
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-15 22:15:31 +0530

    feat(Logger): enhance logging with daily rotation and environment-based log level

--- src/shared/utils/logger.ts
+++ src/shared/utils/logger.ts
@@ -1,18 +1,32 @@
 import { createLogger, format, transports } from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+import { NODE_ENV } from "../constants/env";
 const { combine, timestamp, json, printf, colorize } = format;
+
+
 const consoleLogFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
-const logger = createLogger({
-  level: "info",
-  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), json()),
+const logLevel = NODE_ENV === "production" ? "info" : "debug";
 
+const fileTransport = new DailyRotateFile({
+  filename: "logs/app-%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  zippedArchive: true,
+  maxSize: "20m", 
+  maxFiles: "7d", 
+});
+
+// Create the logger
+const logger = createLogger({
+  level: logLevel,
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), json()),
   transports: [
     new transports.Console({
       format: combine(colorize(), consoleLogFormat),
     }),
-    new transports.File({ filename: "logs/app.log" }),
+    fileTransport,
   ],
 });
 
