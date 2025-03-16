d4beba73b8a8b71c7802b7e364f221075abdcfc7
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-17 01:18:23 +0530

    feat(IReviewsRepository): add updateReview and deleteReview methods for review management

--- src/application/repositories/IReviewsRepository.ts
+++ src/application/repositories/IReviewsRepository.ts
@@ -6,6 +6,8 @@ import { ObjectId } from "../../infrastructure/models/UserModel";
 export interface IReviewsRepository {
   createReview({ userId, doctorId, rating, reviewText }: ReviewsAndRatingParams): Promise<IReview>;
   findAllReviewsByDoctorId(doctorId: ObjectId): Promise<IReview[]>;
+  updateReview({ userId, doctorId, rating, reviewText , reviewId }: ReviewsAndRatingParams): Promise<void>;
+  deleteReview(doctorId: ObjectId, reviewId: ObjectId , userId : ObjectId): Promise<void>;
 }
 
 export const IReviewsRepositoryToken = new Token<IReviewsRepository>();
