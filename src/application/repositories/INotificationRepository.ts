d483fbe1970404a3781bc410847f9dc174bf0c02
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-19 02:36:20 +0530

    feat(INotificationRepository): update getAllNotificationById to include user role parameter

--- src/application/repositories/INotificationRepository.ts
+++ src/application/repositories/INotificationRepository.ts
@@ -7,7 +7,7 @@ export interface INotificationRepository {
   createNotification(notification: Partial<INotification>): Promise<INotification>;
   getAllNotifications(type: string[]): Promise<any>;
   deleteNotification(id: mongoose.Types.ObjectId): Promise<void>;
-  getAllNotificationById(id: mongoose.Types.ObjectId): Promise<INotification[]>;
+  getAllNotificationById(id: mongoose.Types.ObjectId, role: string): Promise<INotification[]>;
   markNotificationAsRead(id: ObjectId): Promise<void>;
 }
 
