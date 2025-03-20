c4ec8b2e47a9ac45aaf554d612621255db48de80
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-21 03:33:50 +0530

    feat(IDoctorRepository): update method signatures to use DoctorDocument and include role in updatePassword

--- src/application/repositories/IDoctorReposirtory.ts
+++ src/application/repositories/IDoctorReposirtory.ts
@@ -5,14 +5,16 @@ import { Doctor } from "../../domain/entities/Doctors";
 import { DoctorDetails } from "../../domain/entities/DoctorDetails";
 import { DisplayDoctorsParams, DoctorDetailsParams, DoctorProfile, DoctorwithDetails, UpdateDoctorParams } from "../../domain/types/doctorTypes";
 import { DoctorDetailsDocument } from "../../infrastructure/models/doctor.details.model";
+import Role from "../../shared/constants/UserRole";
+import { DoctorDocument } from "../../infrastructure/models/DoctorModel";
 
 export interface IDoctorRepository { 
-    findDoctorByEmail(email: string): Promise<Doctor | null>
-    createDoctor(details: Doctor): Promise<Doctor>
+    findDoctorByEmail(email: string): Promise<DoctorDocument | null>
+    createDoctor(details: Partial<DoctorDocument>): Promise<DoctorDocument>
     createDoctorDetails(details:DoctorDetails): Promise<DoctorDetails>
-    findDoctorByID(id: mongoose.Types.ObjectId): Promise<Doctor>
+    findDoctorByID(id: mongoose.Types.ObjectId): Promise<DoctorDocument | null>
     findDoctorDetails(doctorId : mongoose.Types.ObjectId): Promise<DoctorDetails | null>
-    updateUserById(id: mongoose.Types.ObjectId, updates: Partial<Doctor>): Promise<Doctor | null>;
+    updateUserById(id: mongoose.Types.ObjectId, updates: Partial<DoctorDocument>): Promise<DoctorDocument | null>;
     deleteDoctorById(id:mongoose.Types.ObjectId) : Promise<void>;
     deleteDoctorDetails(id:mongoose.Types.ObjectId) : Promise<void>;
     fetchAllDoctors({ page, limit, search, sortBy }: UpdateDoctorParams): Promise<{
@@ -21,7 +23,7 @@ export interface IDoctorRepository {
       }>
     fetchDoctorandDetailsById(id:mongoose.Types.ObjectId) : Promise<DoctorProfile | null>
     updateDoctorDetailsByDocId(userId : mongoose.Types.ObjectId , details :Partial<DoctorDetailsParams> ) : Promise<DoctorDetailsDocument | null>
-    updatePassword(userId : mongoose.Types.ObjectId , newPassword : string) : Promise<void>
+    updatePassword(userId : mongoose.Types.ObjectId , newPassword : string , role : string) : Promise<void>
     
 }
 
