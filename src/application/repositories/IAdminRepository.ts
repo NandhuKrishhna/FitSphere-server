ce7f83b2709864b8635578511c27967a6ffecc17
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-21 03:33:35 +0530

    ``` feat(DoctorUseCase): implement method for resetting doctor passwords ```

--- src/application/repositories/IAdminRepository.ts
+++ src/application/repositories/IAdminRepository.ts
@@ -4,13 +4,14 @@ import { Admin } from "../../domain/entities/Admin";
 import { Doctor } from "../../domain/entities/Doctors";
 import mongoose from "mongoose";
 import { LookUpDoctor } from "../../domain/types/doctorTypes";
+import { DoctorDocument } from "../../infrastructure/models/DoctorModel";
 
 export interface IAdminRepository {
     findAdminByEmail(email: string): Promise<any>;
     getAllUsers(): Promise<User | null>;
     getAllDoctors(): Promise<Doctor | null>;
     approveRequest(id: mongoose.Types.ObjectId): Promise<void>;
-    rejectRequest(id: mongoose.Types.ObjectId): Promise<Doctor | null>;
+    rejectRequest(id: mongoose.Types.ObjectId): Promise<DoctorDocument | null>;
     doctorDetails() : Promise<LookUpDoctor | null>
     unblockById(id : mongoose.Types.ObjectId): Promise<void>;
     blockById(id : mongoose.Types.ObjectId): Promise<User | null>;
