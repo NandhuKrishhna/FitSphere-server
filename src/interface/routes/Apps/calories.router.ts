f12158dc432f46fb51ac531c967290c7a9296823
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-21 03:40:05 +0530

    refactor(calories.router): change update-userdetails route from POST to PATCH for better semantics

--- src/interface/routes/Apps/calories.router.ts
+++ src/interface/routes/Apps/calories.router.ts
@@ -6,7 +6,7 @@ const caloriesRouter = Router();
 const caloriesController = Container.get(CaloriesController);
 caloriesRouter.post("/serach-food", caloriesController.searchRecipeHandler);
 caloriesRouter.post("/generate-recipe", caloriesController.getRecipeHandler);
-caloriesRouter.post("/update-userdetails", caloriesController.updateUserDetails);
+caloriesRouter.patch("/update-userdetails", caloriesController.updateUserDetails);
 caloriesRouter.post("/add-foodlog", caloriesController.addMealHandler);
 caloriesRouter.post("/delete-food", caloriesController.deleteFoodHandler);
 caloriesRouter.post("/get-foodlog", caloriesController.getFoodLogsHandler);
