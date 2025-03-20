c5fd010fc1b13dc3ec2d6f62e9ebe1856890d60f
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-21 03:34:00 +0530

    feat(IUserRepository): update method signatures to use UserDocument for user operations

--- src/application/repositories/IUserRepository.ts
+++ src/application/repositories/IUserRepository.ts
@@ -2,15 +2,16 @@
 import { Token } from "typedi";
 import { User, UserType } from "../../domain/entities/User";
 import mongoose from "mongoose";
+import { UserDocument } from "../../infrastructure/models/UserModel";
 
 export interface IUserRepository {
-  createUser(user: UserType): Promise<UserType>;
-  findUserByEmail(email: string): Promise<UserType | null>;
+  createUser(user: Partial<UserDocument>): Promise<UserDocument>;
+  findUserByEmail(email: string): Promise<UserDocument | null>;
   updateUserStatus(email: string, isActive: boolean): Promise<void>;
-  updateUserById(id: mongoose.Types.ObjectId, updates: Partial<UserType>): Promise<UserType | null>;
-  updateUserByEmail(email: string, updates: Partial<UserType>): Promise<UserType | null>;
-  findUserById(id: mongoose.Types.ObjectId): Promise<UserType | null>;
-  updateProfile(userId: mongoose.Types.ObjectId, profilePic: string): Promise<UserType | null>;
+  updateUserById(id: mongoose.Types.ObjectId, updates: Partial<UserDocument>): Promise<UserDocument | null>;
+  updateUserByEmail(email: string, updates: Partial<UserDocument>): Promise<UserDocument | null>;
+  findUserById(id: mongoose.Types.ObjectId): Promise<UserDocument | null>;
+  updateProfile(userId: mongoose.Types.ObjectId, profilePic: string): Promise<UserDocument | null>;
 }
 
 export const IUserRepositoryToken = new Token<IUserRepository>();
