ede51d5d4bad670e0ca897029ddb95809ba0cb0a
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-28 03:30:07 +0530

    feat(notification): enhance notification repository methods with detailed return types

--- src/application/repositories/INotificationRepository.ts
+++ src/application/repositories/INotificationRepository.ts
@@ -2,12 +2,18 @@ import { Token } from "typedi";
 import mongoose from "mongoose";
 import { INotification } from "../../infrastructure/models/notification.models";
 import { ObjectId } from "../../infrastructure/models/UserModel";
+import { NotificationQueryParams } from "../../domain/types/queryParams.types";
 
 export interface INotificationRepository {
   createNotification(notification: Partial<INotification>): Promise<INotification>;
-  getAllNotifications(type: string[]): Promise<any>;
+  getAllNotifications(type: string[]): Promise<INotification[]>;
   deleteNotification(id: mongoose.Types.ObjectId): Promise<void>;
-  getAllNotificationById(id: mongoose.Types.ObjectId, role: string): Promise<INotification[]>;
+  getAllNotificationById(id: mongoose.Types.ObjectId, role: string, queryParams: NotificationQueryParams): Promise<{
+    notifications: INotification[];
+    currentPage: number;
+    totalPages: number;
+    totalNotifications: number;
+  }>;
   markNotificationAsRead(id: ObjectId): Promise<void>;
 }
 
