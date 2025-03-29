9ec7592fdb39d8ba65e98872f06782636aa661f5
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-30 01:51:46 +0530

    refactor(auth): remove unused doctor authentication middleware and notification router

--- src/interface/routes/Apps/notificatation.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import Container from "typedi";
-import { AppController } from "../../controllers/Feat/AppController";
-
-const notificationRouter = Router();
-const notificationController = Container.get(AppController);
-notificationRouter.get("/", notificationController.getNotificationsHandler);
-
-export default notificationRouter;
