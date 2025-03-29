a6631f151bad03a36e734f5e80cab08ca2d18515
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-30 01:59:14 +0530

    refactor(logger): simplify logger configuration for development and production environments

--- src/shared/utils/logger.ts
+++ src/shared/utils/logger.ts
@@ -1,33 +1,34 @@
 import { createLogger, format, transports } from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
 import { NODE_ENV } from "../constants/env";
-const { combine, timestamp, json, printf, colorize } = format;
 
+const { combine, timestamp, json, printf, colorize } = format;
 
 const consoleLogFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
-const logLevel = NODE_ENV === "production" ? "info" : "debug";
-
 const fileTransport = new DailyRotateFile({
   filename: "logs/app-%DATE%.log",
   datePattern: "YYYY-MM-DD",
   zippedArchive: true,
-  maxSize: "20m", 
-  maxFiles: "7d", 
+  maxSize: "20m",
+  maxFiles: "7d",
 });
 
-// Create the logger
-const logger = createLogger({
-  level: logLevel,
-  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), json()),
-  transports: [
-    new transports.Console({
-      format: combine(colorize(), consoleLogFormat),
-    }),
-    fileTransport,
-  ],
-});
+const logger = NODE_ENV === "development"
+  ? createLogger({
+    level: "debug",
+    format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), json()),
+    transports: [
+      new transports.Console({
+        format: combine(colorize(), consoleLogFormat),
+      }),
+      fileTransport,
+    ],
+  })
+  : createLogger({
+    transports: [],
+  });
 
 export default logger;
