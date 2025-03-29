faf27709d695226cf36d230ff70d071a7a9ec55e
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-30 01:51:27 +0530

    feat(subscription): add methods for fetching and managing user subscriptions in the repository

--- src/application/repositories/IUserSubscriptionRepository.ts
+++ src/application/repositories/IUserSubscriptionRepository.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 import { Token } from "typedi";
 import { AddSubscriptionParams } from "../../infrastructure/repositories/UserSubscription";
+import { IUserSubscription } from "../../infrastructure/models/userSubscription-model";
 
 export interface IUserSubscriptionRepository {
     addSubscription({ userId, subscriptionId, paymentId }: AddSubscriptionParams): Promise<void>
+    getSubscriptionDetails(userId: mongoose.Types.ObjectId): Promise<any | null>;
+    createDefaultSubscription(userId: mongoose.Types.ObjectId): Promise<void>;
+    deleteExistingSubscription({ userId }: { userId: mongoose.Types.ObjectId }): Promise<void>;
 }
 export const IUserSubscriptionRepositoryToken = new Token<IUserSubscriptionRepository>();
