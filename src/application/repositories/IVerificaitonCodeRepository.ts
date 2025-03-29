eb9a4e2a533ab7eab09080e20e3e2fe4c526431c
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-30 01:53:04 +0530

    feat(types): add new types for token payload, doctor use case, payment parameters, and transaction details

--- src/application/repositories/IVerificaitonCodeRepository.ts
+++ src/application/repositories/IVerificaitonCodeRepository.ts
@@ -1,12 +1,12 @@
 import { Token } from "typedi";
 import { VerificationCode } from "../../domain/entities/VerificationCode";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 export interface IVerficaitonCodeRepository {
-    createVerificationCode(code :VerificationCode): Promise<VerificationCode>;
-    findVerificationCode(  code: string, type:string ): Promise<VerificationCode | null>;
+    createVerificationCode(code: VerificationCode): Promise<VerificationCode>;
+    findVerificationCode(code: string, type: string): Promise<VerificationCode | null>;
     deleteVerificationCode(id: mongoose.Types.ObjectId): Promise<void>;
-    countVerificationCodes(id: mongoose.Types.ObjectId, type: string , time: Date): Promise<number>    
+    countVerificationCodes(id: mongoose.Types.ObjectId, type: string, time: Date): Promise<number>
 
 }
 
