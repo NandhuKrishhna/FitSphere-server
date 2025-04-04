691fd7c6cb7a91891805d6596d7916bdf0c9a488
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-05 00:13:56 +0530

    feat: update conversation types to make updatedAt optional and enhance ConversationResponse structure

--- src/domain/types/conversation.types.ts
+++ src/domain/types/conversation.types.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from "../../infrastructure/models/UserModel";
+
 type DoctorDetails = {
     _id: string;
     name: string;
@@ -6,9 +8,16 @@ type DoctorDetails = {
 
 type User = {
     _id: string;
-    updatedAt: string;
+    updatedAt?: string;
     lastMessage: string;
     doctorDetails: DoctorDetails;
 };
 
 export type QueryResult = User[];
+
+
+export type ConversationResponse = {
+    _id: ObjectId,
+    doctorDetails: DoctorDetails,
+    lastMessage: string
+}
